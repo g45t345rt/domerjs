1c6a5680b1c98d71dd81fee5ed43aee97b68a0a7
g45t345rt <fred10.cha@gmail.com>
2020-12-22 14:25:40 -0500

    new src/index export

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import * as router from './router'
-import * as fetcher from './fetcher'
-import * as helpers from './helpers'
-import { newEl, updateEl, newElClass, setElKey, update, updateSet } from './element'
-
-import SSR from './ssr'
-
-export {
-  // element
-  newEl,
-  newElClass,
-  updateEl,
-  update,
-  updateSet,
-  setElKey,
-  // others
-  router,
-  fetcher,
-  helpers,
-  SSR
-}
-
+export * from './element'
+export * as router from './router'
+export * as fetcher from './fetcher'
+export * as helpers from './helpers'
+export * as SSR from './ssr'
